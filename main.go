d3f1151ffb5b756e565d864e1f9e4d193f1e6be7
Richard Tang <115213709+richardtlp@users.noreply.github.com>
2023-03-19 13:21:54 +0700

    Fault tolerant multi-node solution (#1)
    * add context timeout
    
    * use set to reduce duplicate message checking time

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
 	"sync"
+	"time"
 )
 
 type broadcastBody struct {
@@ -18,9 +19,12 @@ type topologyBody struct {
 	Topology map[string][]string `json:"topology"`
 }
 
+var member struct{}
+
 type server struct {
-	numbers  []float64
-	topology map[string][]string
+	numbers    []float64
+	setNumbers map[float64]struct{}
+	topology   map[string][]string
 
 	n          *maelstrom.Node
 	numberLock sync.RWMutex
@@ -29,6 +33,7 @@ type server struct {
 func main() {
 	s := server{
 		numbers:    make([]float64, 0),
+		setNumbers: make(map[float64]struct{}),
 		topology:   make(map[string][]string),
 		n:          maelstrom.NewNode(),
 		numberLock: sync.RWMutex{},
@@ -73,21 +78,39 @@ func (s *server) broadcastHandler(msg maelstrom.Message) error {
 }
 
 func storeAndBroadcastMessage(s *server, body broadcastBody, msg maelstrom.Message) {
-	if !isMessageExisted(s.numbers, body.Message) {
-		s.numberLock.Lock()
-		s.numbers = append(s.numbers, body.Message)
-		s.numberLock.Unlock()
+	if !isMessageExisted(s, body.Message) {
+		storeMessage(s, body)
 		for _, dest := range s.topology[msg.Dest] {
-			_, _ = s.n.SyncRPC(context.Background(), dest, body)
+			go broadcastWhileTimeout(s, dest, body)
 		}
 	}
 }
 
-func isMessageExisted(numbers []float64, message float64) bool {
-	for _, num := range numbers {
-		if num == message {
-			return true
+func isMessageExisted(s *server, message float64) bool {
+	s.numberLock.RLock()
+	_, ok := s.setNumbers[message]
+	s.numberLock.RUnlock()
+	return ok
+}
+
+func storeMessage(s *server, body broadcastBody) {
+	s.numberLock.Lock()
+	s.numbers = append(s.numbers, body.Message)
+	s.setNumbers[body.Message] = member
+	s.numberLock.Unlock()
+}
+
+func broadcastWhileTimeout(s *server, dest string, body broadcastBody) {
+	for {
+		if broadcast(s, dest, body) == nil {
+			break
 		}
 	}
-	return false
+}
+
+func broadcast(s *server, dest string, body broadcastBody) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err := s.n.SyncRPC(ctx, dest, body)
+	return err
 }
